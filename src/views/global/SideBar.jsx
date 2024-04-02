341bfbc260754c0d12b7499a804418d8a977f586
Matt Porter <mattyp.ryan@gmail.com>
2024-04-01 19:10:30 -0700

    Began constructing react pro sidebar

--- src/views/global/SideBar.jsx
+++ src/views/global/SideBar.jsx
@@ -1,4 +1,76 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import HelplinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+
 const SideBar = () => {
-    return <div>SideBar</div>;}
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const [ selected, setSelected ] = useState("Dashboard")
+    
+    
+    
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`
+                },
+                "& .pro-icon-wrapper" : {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important"
+                }
+            }}  
+        >
+
+
+            {/* USER */}
+            {!isCollapsed && (
+                <Box mb="25px">
+                    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                        <img 
+                        alt="Profile User"
+                        width={"100px"}
+                        height={"100px"}
+                        src="../../../public/assets/Matt-2.jpg"
+                        style={{cursor: "pointer", borderRadius:"50%"}}
+                        />
+                    </Box>
+
+                    <Box textAlign={"center"}>
+                        <Typography variant="h2" color={colors.grey[100]} fontWeight={"bold"} sx={{ m: "10px 0 0 0 "}}>Ed Roh</Typography>
+                        <Typography variant = "h5" color={colors.greenAccent[500]}>Fancy Admin Guy</Typography>
+                    </Box>
+                </Box>
+            )}
+        </Box>
+    )
+
+}
 
 export default SideBar;
